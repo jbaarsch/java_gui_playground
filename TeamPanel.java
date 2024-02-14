8b7d171a1097cf7f908b876f99d54dc0523199e5
Okiilsgaard <okiilsgaard@cub.uca.edu>
2024-02-14 16:14:15 -0600

    Final Changes

--- TeamPanel.java
+++ TeamPanel.java
@@ -17,7 +17,11 @@ public class TeamPanel extends JPanel {
         memberLabels[1] = new JLabel("Owen", new ImageIcon("BruceBear.png"), SwingConstants.CENTER);
         memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[1].setVerticalTextPosition(SwingConstants.BOTTOM);
+<<<<<<< Updated upstream
         memberLabels[2] = new JLabel();
+=======
+        memberLabels[2] = new JLabel("Canon", new ImageIcon("fries.jpg"), SwingConstants.CENTER);
+>>>>>>> Stashed changes
 
 
 
@@ -25,7 +29,11 @@ public class TeamPanel extends JPanel {
             this.add(member);
         }
 
+<<<<<<< Updated upstream
         this.setPreferredSize(new Dimension(850, 500));
+=======
+        this.setPreferredSize(new Dimension(750, 1000));
+>>>>>>> Stashed changes
         this.setBackground(Color.LIGHT_GRAY);
 
 
