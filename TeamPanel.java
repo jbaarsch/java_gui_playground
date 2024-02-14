27a3ea901eb2a6f9a386bf8862f067c4a9e03724
KevinS1120 <kstinnett@cub.uca.edu>
2024-02-14 15:52:08 -0600

    Bug picture

--- TeamPanel.java
+++ TeamPanel.java
@@ -15,7 +15,9 @@ public class TeamPanel extends JPanel {
         memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
         memberLabels[1] = new JLabel();
-        memberLabels[2] = new JLabel();
+        memberLabels[2] = new JLabel("Kevin", new ImageIcon("insect.png"), SwingConstants.RIGHT);
+        memberLabels[2].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[2].setVerticalTextPosition(SwingConstants.BOTTOM);
 
 
 
@@ -23,7 +25,7 @@ public class TeamPanel extends JPanel {
             this.add(member);
         }
 
-        this.setPreferredSize(new Dimension(500, 250));
+        this.setPreferredSize(new Dimension(500, 500));
         this.setBackground(Color.LIGHT_GRAY);
 
 
