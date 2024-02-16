89c1857ce785c2f56eadef72685dff99aaa49c1e
Dakotah Sutton Rodriguez <dakotahsuttonrodriguez@gmail.com>
2024-02-16 10:12:26 -0600

    Added picture of donkey

--- TeamPanel.java
+++ TeamPanel.java
@@ -11,11 +11,15 @@ public class TeamPanel extends JPanel {
         memberLabels = new JLabel[TEAM_SIZE];
 
         // create your own label that have an image and your name as text.
-        memberLabels[0] = new JLabel("Sample", new ImageIcon("smile.png"), SwingConstants.CENTER);
-        memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[0] = new JLabel("Malenia, Blade of Miquella", new ImageIcon("Malenia.jpeg"), SwingConstants.LEFT);
+        memberLabels[1] = new JLabel("Dakotah Sample", new ImageIcon("funny-animal-picture-hnczwmybpoojbyse.jpg"), SwingConstants.CENTER);
+        memberLabels[2] = new JLabel("Ethan Sample", new ImageIcon("TonyHappy.jpg"), SwingConstants.RIGHT);
+        memberLabels[0].setHorizontalTextPosition(SwingConstants.LEFT);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[1] = new JLabel();
-        memberLabels[2] = new JLabel();
+        memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[1].setVerticalTextPosition(SwingConstants.BOTTOM);
+        memberLabels[2].setHorizontalTextPosition(SwingConstants.RIGHT);
+        memberLabels[2].setVerticalTextPosition(SwingConstants.BOTTOM);
 
 
 
